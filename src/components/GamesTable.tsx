ed839eb9f8d64b98633272d08fa85afefce9d07f
Adrian David <dreiclark@gmail.com>
2022-03-13 21:37:53 +0000

    removed score column, added hyperlink to nbareplay

--- src/components/GamesTable.tsx
+++ src/components/GamesTable.tsx
@@ -1,25 +1,12 @@
-import { Table, TableBody, TableCell, TableContainer, TableHead, TableRow } from "@mui/material";
+import { Link, Table, TableBody, TableCell, TableContainer, TableHead, TableRow } from "@mui/material";
 import { GameData } from "../interfaces/GameData";
 
-export default function GamesTable(props: {games: GameData[]}) {
-    const shuffleArray = (array: any[]) => {
-        for (let i = array.length - 1; i > 0; i--) {
-          const j = Math.floor(Math.random() * (i + 1));
-          const temp = array[i];
-          array[i] = array[j];
-          array[j] = temp;
-        }
-    }
-
-    const shuffleAndStringifyScores = (game: GameData): string => {
-        let scores = [game["home_team_score"], game["visitor_team_score"]]
-        shuffleArray(scores)
-
-        return scores.join(" - ")
-    }
-
+export default function GamesTable(props: { games: GameData[] }) {
     const removeTimestampFromDate = (datestr: string): string => datestr.split('T')[0]
-    
+    const constructGameKey = (game: GameData): string => game["home_team"]["abbreviation"] + game["visitor_team"]["abbreviation"] + game["date"]
+    const toKebabCase = (s: string): string => s.toLowerCase().replaceAll(' ', '-');
+    const constructGameLink = (game: GameData): string => `https://nbareplay.net/category/${toKebabCase(game["home_team"]["full_name"])}`
+
     return (
         <TableContainer>
             <Table>
@@ -27,19 +14,23 @@ export default function GamesTable(props: {games: GameData[]}) {
                     <TableRow>
                         <TableCell><strong>Date</strong></TableCell>
                         <TableCell><strong>Game</strong></TableCell>
-                        <TableCell><strong>Score</strong></TableCell>
                     </TableRow>
                 </TableHead>
                 <TableBody>
-                    { props.games.map((game: any) => 
-                    <TableRow key={game["home_team"]["abbreviation"] + game["visitor_team"]["abbreviation"] + game["date"]}>
-                        <TableCell>{removeTimestampFromDate(game["date"])}</TableCell>
-                        <TableCell>{game["home_team"]["full_name"]} vs {game["visitor_team"]["full_name"]}</TableCell>
-                        <TableCell>{shuffleAndStringifyScores(game)}</TableCell>
-                    </TableRow>) 
+                    {props.games.map((game: GameData) =>
+
+                        <TableRow hover key={constructGameKey(game)}>
+                            <TableCell>{removeTimestampFromDate(game["date"])}</TableCell>
+                            <TableCell>
+                                <Link href={constructGameLink(game)} underline="none" color="white">
+                                    {game["home_team"]["full_name"]} vs {game["visitor_team"]["full_name"]}
+                                </Link>
+                            </TableCell>
+                        </TableRow>
+                    )
                     }
-                </TableBody>    
+                </TableBody>
             </Table>
         </TableContainer>
-  )
+    )
 }
